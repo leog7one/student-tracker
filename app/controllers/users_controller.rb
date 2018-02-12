ce0bd96058fbb8528ae8a5b8947649bc1bde6c53
leog7one <lguzman@me.com>
2018-02-12 16:37:19 -0600

    linked users show page in admin panel, updated users show page to show referrals per user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@referral = Referral.find(params[:id])
-		@referrals = Referral.all
+		@referrals = @user.referrals
 	end
 end

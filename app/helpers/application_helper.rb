ad2fc270c79102fdcc5a74a6ce60842a40dd0869
leog7one <lguzman@me.com>
2018-03-18 21:48:00 -0500

    updating views to refelect numbers for better understanding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def time_format(referral_date)
-		referral_date.strftime("%A, %b %e %Y %l:%M %p ")
+		referral_date.strftime("%A, %b %e %Y at %l:%M %p ")
   end
   
 end
